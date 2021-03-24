3d32f91ee9b95d12b9b1c41f73e7e4b82b7763b1
samycharas <samy.charas@cpe.fr>
2021-03-24 10:00:11 +0100

    Removing DFFR

--- techlibs/quicklogic/qlf_k6n10_cells_sim.v
+++ techlibs/quicklogic/qlf_k6n10_cells_sim.v
@@ -76,22 +76,6 @@ module dff(
             Q <= D;
 endmodule
 
-module dffr(
-    output reg Q,
-    input D,
-    (* clkbuf_sink *)
-    input C,
-    input R
-);
-    parameter [0:0] INIT = 1'b0;
-    initial Q = INIT;
-
-    always @(posedge C or negedge R)
-        if (!R)
-            Q <= 1'b0;
-        else 
-            Q <= D;
-endmodule
 
 (* abc9_flop, lib_whitebox *)
 module scff(
--- techlibs/quicklogic/qlf_k6n10_ffs_map.v
+++ techlibs/quicklogic/qlf_k6n10_ffs_map.v
@@ -4,20 +4,3 @@ module \$_DFF_P_ (D, Q, C);
     output Q;
     dff _TECHMAP_REPLACE_ (.Q(Q), .D(D), .C(C));
 endmodule
-
-module \$_DFF_PN0_ (D, Q, C, R);
-    input D;
-    input C;
-    input R;
-    output Q;
-    dffr _TECHMAP_REPLACE_ (.Q(Q), .D(D), .C(C), .R(R));
-endmodule
-
-module \$_DFF_PP0_ (D, Q, C, R);
-    input D;
-    input C;
-    input R;
-    output Q;
-    dffr _TECHMAP_REPLACE_ (.Q(Q), .D(D), .C(C), .R(!R));
-endmodule
-
